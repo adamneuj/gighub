730f201b98abd17479f6a108ea640bfc198ef7ee
Adam Neujahr <adamneujahr@gmail.com>
2021-01-13 16:14:41 -0600

    Add react-router-dom and links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Navigation from './navigation/navigation';
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
+import Home from './home/home';
+import Login from './login/login';
 
 class App extends Component {
   render() {
     return (
-      <Navigation />
+      <main>
+        <Navigation />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </main>
     );
   }
 }
--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './navigation.css';
 
 class Navigation extends Component {
     render() {
         const loggedIn = this.checkLoggedIn();
         if(loggedIn){
             return (
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">GigHub</Navbar.Brand>
+                <Navbar bg="dark">
+                    <Navbar.Brand><Link to='/' className="link">GigHub</Link></Navbar.Brand>
                     <Nav className="mr-auto">
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#login">Logout</Nav.Link>
+                        <Nav.Link><Link to='/login' className="link">Logout</Link></Nav.Link>
                     </Nav>
                 </Navbar>
             );
         } else {
             return (
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">GigHub</Navbar.Brand>
+                <Navbar bg="dark">
+                    <Navbar.Brand><Link to='/' className="link">GigHub</Link></Navbar.Brand>
                     <Nav className="mr-auto">
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#login">Login</Nav.Link>
+                        <Nav.Link><Link to='/login' className="link">Login</Link></Nav.Link>
                     </Nav>
                 </Navbar>
             );
         }
-
     }
 
     checkLoggedIn() {
